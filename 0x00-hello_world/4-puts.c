f92f55f66444ba6412c2192eebb5df990a7e9fe9
mohamed <mohamed@gmail.com>
2023-03-16 17:47:15 -0700

    Second commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing statment with puts function
+ *
+ * Return: 0 ==> (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+{
