9ee3b87953989c5cb6863839621df048ad3737bf
mohamed <mohamed@gmail.com>
2023-03-22 14:44:50 -0700

    Seconf commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: C programm prints alphabet using prototype
+ * Return: Always 0 sucess)
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+
+	return (0);
+}
