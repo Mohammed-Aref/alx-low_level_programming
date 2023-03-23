6e23c200e494401ff5ce0640b6ae84f589a9ed87
mohamed <mohamed@gmail.com>
2023-03-23 08:56:06 -0700

    Fifth commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_most_numbers- prints numbers from 0-9 followed by a new line except 2,4
+ *
+ * Return: no return value
+*/
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n >= 48; n <= 57; n++)
+	{
+		if (n == 50 || n == 52)
+			continue;
+		else
+			_putchar(n);
+	}
+	_putchar('\n');
+}
