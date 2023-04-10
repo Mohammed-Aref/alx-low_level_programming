6750207efc989e855c7089479a35529071b78455
mohamed <mohamed@gmail.com>
2023-04-10 15:43:21 -0700

    Second commit fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,8 @@
  * @argv : array pointer
  * Return: (0)
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	(void)argc;
 	printf("%d\n", argc - 1);
 	return (0);
 }
